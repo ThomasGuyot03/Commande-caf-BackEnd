98cab485a6a5997ce0374c8d6b61958d5a091109
Guyot <thomasguyot31@live.fr>
2024-10-25 16:56:56 +0200

    .

--- middleware/account.js
+++ middleware/account.js
@@ -1,8 +1,9 @@
 const findAccount = (origin) => {
     let accountId 
+    console.log(origin.headers.origin)
     if (origin.headers.origin === "http://thomasguyot.local:8080") {
         accountId = "66e964f0c8bf10e1ef23bb9b" 
-    } else if (origin.headers.origin === "http://commandedat.netlify.app") {
+    } else if (origin.headers.origin === "https://commandedat.netlify.app") {
         accountId = "66e964aec8bf10e1ef23bb9a"
     }
     return accountId    
