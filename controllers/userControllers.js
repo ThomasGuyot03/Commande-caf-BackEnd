dd4f403dfbcf097cc410b6cc9dfbde68e4fdf983
Guyot <thomasguyot31@live.fr>
2025-03-06 11:11:15 +0100

    Suppréssion caractère spéciaux pour le mdp

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,7 +5,7 @@ const { getTransporterMail } = require("../middleware/function")
 const { findAccount } = require("../middleware/account")
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!.@#$%^&*])(?=.{8,})/
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/
 
 
 // SIGNUP //
